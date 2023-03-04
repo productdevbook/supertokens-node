cb8e1c28e4a5ed58b1922675f6d7c7effda81166
productdevbook <hi@productdevbook.com>
2023-03-04 11:35:50 +0300

    feat: tsup import

--- tsup.config.ts
+++ tsup.config.ts
@@ -13,18 +13,33 @@ export default <Options>{
         'src/types.ts',
 
         'src/recipe/dashboard/index.ts',
+
         'src/recipe/emailpassword/index.ts',
+        'src/recipe/emailpassword/emaildelivery/index.ts',
         'src/recipe/emailverification/index.ts',
+        'src/recipe/emailverification/emaildelivery/index.ts',
+
         'src/recipe/jwt/index.ts',
         'src/recipe/openid/index.ts',
+
         'src/recipe/passwordless/index.ts',
-        'src/recipe/session/framework/**/index.ts',
+        'src/recipe/passwordless/emaildelivery/index.ts',
+        'src/recipe/passwordless/smsdelivery/index.ts',
+        
+        'src/recipe/session/framework/**',
         'src/recipe/session/claims.ts',
         'src/recipe/session/index.ts',
+
         'src/recipe/thirdparty/index.ts',
         'src/recipe/thirdparty/providers/**',
+
         'src/recipe/thirdpartyemailpassword/index.ts',
+        'src/recipe/thirdpartyemailpassword/emaildelivery/index.ts',
+
         'src/recipe/thirdpartypasswordless/index.ts',
+        'src/recipe/thirdpartypasswordless/emaildelivery/index.ts',
+        'src/recipe/thirdpartypasswordless/smsdelivery/index.ts',
+        
         'src/recipe/usermetadata/index.ts',
         'src/recipe/userroles/index.ts',
     ],
