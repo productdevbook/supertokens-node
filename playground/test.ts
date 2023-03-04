2b2c4454e4a251947067f2478437a613ed8a4f6f
productdevbook <hi@productdevbook.com>
2023-03-04 11:44:07 +0300

    Update test.ts

--- playground/test.ts
+++ playground/test.ts
@@ -1,5 +1,6 @@
 import { SessionRequest } from 'supertokens-node/framework/fastify'
 import {init } from 'supertokens-node'
 import Passwordless from "supertokens-node/recipe/passwordless";
+import {APIHandled} from "supertokens-node/types";
 
 import {superTokensNextWrapper} from "supertokens-node/nextjs";
